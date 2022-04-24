6ab1c25893019b175a0f5c363f676a81facf4fff
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-04-24 02:47:36 -0700

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concanates two strings,
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
  * @n:amount of bytes used from src.
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
-	while (8(dest + count) != '\0')
+	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
