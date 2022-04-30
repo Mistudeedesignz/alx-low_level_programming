8a4db4f5c30f165b796e0a4025cb70a870bff0e6
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-04-29 17:21:47 -0700

    commit 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int power_operation(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
-		if (c * (n / c) == n )
+		if (c * (n / c) == n)
 			return (c);
 		else
 			return (-1);
@@ -31,5 +31,4 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 	return (power_operation(n, 2));
-}
-		
+}		
