c8b9f11591e01c29c0646f3d40197e9bd81c19e0
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-05-04 10:09:05 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @main - This prints the name of the programme
+ * @argc: The number of the arguments
+ * @argv: An array of the arguments passed
+ * Return: 0
+ * */
+
+int main(int argc, char **argc)
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argc[0]);
+	}
+	return (0);
+}
