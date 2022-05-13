e2a8856292ebcb11e58ab4262298d06183541dab
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-05-13 08:44:36 -0700

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - a function that prints a name
  * @name: the name of the person to be printed
- * @f: a pointer to  a function that prints a name
+ * @f: a pointer to a function that prints a name
  */
 
 void print_name(char *name, void (*f)(char *))
