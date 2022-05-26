d17040b8737b3e63409a58d2e524390829dfdb35
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-05-26 16:35:33 -0700

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,11 +12,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *temp = *head;
 
-	new = maoc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
-	new-> = n;
+	new->n = n;
 	new->next = NULL;
 
 	if  (*head == NULL)
