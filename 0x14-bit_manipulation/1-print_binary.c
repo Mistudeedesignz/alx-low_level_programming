23db7bd49b9c7ce63707d7a0a040f564b412b7cc
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-05-30 21:55:36 -0700

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,10 +4,10 @@
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
-void print_binary(unsigned long int b)
+void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsined long int currrent;
+	unsigned long int currrent;
 
 	for (i = 63; i >= 0; i--)
 	{
