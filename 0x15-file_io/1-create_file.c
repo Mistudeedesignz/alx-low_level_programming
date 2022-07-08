bfeab218cc37fee9a930ab2cd0763aecc828b7d3
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-07-07 17:49:05 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,28 +6,26 @@
  * @text_content: text to write in the file 
  *
  * Return: 1 on success, -1 on failure
- */
+ **/
 int create_file(const char *filename, char *text_content)
 {
-	int fd, t, s = 0;
+	int fd, t, res;
 
-	if (!filename)
+	if (!filenam == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 
-	if (text_context)
-	{
-		while (text_content[s])
-			s++;
-		t = write(fd, text_content, s);
-		if (t != s)
-			return (-1);
-	}
+	if (text_content == NULL)
+		return (1);
 
-	close(fd);
+	for (i = 0; text_content[i] != 0; i++)
+		;
 
-	return (1);
+	res = write(fd, text_content, i);
+	close(fd);;
+
+	return (res == -1 ? -1 : 1);
 }
